cff1247333572e3e9e18d1c8f25a31d66056798a
Christopher Milne-O'Grady <cfogrady@gmail.com>
2023-03-27 22:20:53 -0400

    Add previews.

--- app/src/main/java/com/github/cfogrady/vitalwear/LoadingComposable.kt
+++ app/src/main/java/com/github/cfogrady/vitalwear/LoadingComposable.kt
@@ -1,32 +1,40 @@
 package com.github.cfogrady.vitalwear
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Text
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlin.coroutines.coroutineContext
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.ImageDecoderDecoder
 
 const val LOADING_TEXT = "Loading..."
 
 @Composable
 fun Loading(work: () -> Unit) {
+    val imageLoader = ImageLoader.Builder(LocalContext.current).components {
+        add(ImageDecoderDecoder.Factory())
+    }.build()
+
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
-        .fillMaxWidth()
-        .padding(20.dp)) {
+            .fillMaxWidth()
+            .padding(20.dp))
+    {
         Text(text = LOADING_TEXT)
+        Image(
+            painter = rememberAsyncImagePainter(R.drawable.loading_icon, imageLoader),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
+        )
     }
     LaunchedEffect(key1 = LOADING_TEXT) {
         withContext(Dispatchers.Default) {
