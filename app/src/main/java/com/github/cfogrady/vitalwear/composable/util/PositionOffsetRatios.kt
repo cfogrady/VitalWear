d8ca116317d49ae5a53ef349c9405ff56b67ad24
Christopher O'Grady <cfogrady@gmail.com>
2023-05-04 23:15:10 -0400

    Fixed battle animations (#28)

--- app/src/main/java/com/github/cfogrady/vitalwear/composable/util/PositionOffsetRatios.kt
+++ app/src/main/java/com/github/cfogrady/vitalwear/composable/util/PositionOffsetRatios.kt
@@ -3,9 +3,9 @@ package com.github.cfogrady.vitalwear.composable.util
 class PositionOffsetRatios {
     companion object {
         const val CHARACTER_OFFSET_FROM_BOTTOM = -0.05f
-        const val CHARACTER_JUMP_BACK_POSITION = .35f
-        const val ATTACK_OFFSET_FROM_BOTTOM = -.25f
-        const val ATTACK_OFFSET_FROM_CENTER = -.15f
+        const val CHARACTER_JUMP_BACK_POSITION = .2f
+        const val ATTACK_OFFSET_FROM_BOTTOM = -.15f
+        const val ATTACK_OFFSET_FROM_CENTER = -.1f
         const val HEALTH_OFFSET_FROM_TOP = .1f
     }
 }
