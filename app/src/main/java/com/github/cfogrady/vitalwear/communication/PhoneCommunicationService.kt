fbaa9f9243f398d6683c9b4dabe11d79729f7984
cfogrady <cfogrady@gmail.com>
2024-01-26 23:10:23 -0500

    Remove unused imports

--- app/src/main/java/com/github/cfogrady/vitalwear/communication/PhoneCommunicationService.kt
+++ app/src/main/java/com/github/cfogrady/vitalwear/communication/PhoneCommunicationService.kt
@@ -4,17 +4,10 @@ import android.util.Log
 import com.github.cfogrady.vitalwear.VitalWearApp
 import com.github.cfogrady.vitalwear.common.communication.ChannelTypes
 import com.google.android.gms.wearable.ChannelClient
-import com.google.android.gms.wearable.MessageEvent
-import com.google.android.gms.wearable.Wearable
 import com.google.android.gms.wearable.WearableListenerService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
-import java.io.ByteArrayOutputStream
-import java.io.InputStream
-import java.nio.charset.Charset
 
 class PhoneCommunicationService  : WearableListenerService() {
     companion object {
