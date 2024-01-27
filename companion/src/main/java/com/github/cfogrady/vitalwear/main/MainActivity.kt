51618e018abebe33b28b9258d31b4a056b5e2b3d
cfogrady <cfogrady@gmail.com>
2024-01-26 23:12:20 -0500

    Remove unused imports.

--- companion/src/main/java/com/github/cfogrady/vitalwear/main/MainActivity.kt
+++ companion/src/main/java/com/github/cfogrady/vitalwear/main/MainActivity.kt
@@ -2,8 +2,6 @@ package com.github.cfogrady.vitalwear.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -15,19 +13,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.cfogrady.vb.dim.card.DimWriter
 import com.github.cfogrady.vitalwear.common.commonLog
 import com.github.cfogrady.vitalwear.card.ImportCardActivity
-import com.github.cfogrady.vitalwear.common.communication.ChannelTypes
 import com.github.cfogrady.vitalwear.firmware.FirmwareImportActivity
-import com.google.android.gms.wearable.Wearable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
-import java.io.IOException
-import java.nio.charset.Charset
 
 class MainActivity : ComponentActivity() {
     companion object {
