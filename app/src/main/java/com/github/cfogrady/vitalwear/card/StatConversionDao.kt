129d56601b0e8bbdf7e019b20370f52579bfa3ab
cfogrady <cfogrady@gmail.com>
2024-01-29 11:48:01 -0500

    Fix missing dao methods

--- app/src/main/java/com/github/cfogrady/vitalwear/card/StatConversionDao.kt
+++ app/src/main/java/com/github/cfogrady/vitalwear/card/StatConversionDao.kt
@@ -2,6 +2,8 @@ package com.github.cfogrady.vitalwear.card
 
 import androidx.room.Dao
 import androidx.room.Query
+import com.github.cfogrady.vitalwear.common.card.CardType
+import com.github.cfogrady.vitalwear.common.card.db.CardMetaEntity
 import com.github.cfogrady.vitalwear.common.card.db.SpeciesEntity
 import com.github.cfogrady.vitalwear.common.card.db.SpecificFusionEntity
 import com.github.cfogrady.vitalwear.common.card.db.TransformationEntity
@@ -12,10 +14,20 @@ interface StatConversionDao {
     @Query("select max(s.phase) from ${TransformationEntity.TABLE} as t " +
             "inner join ${SpeciesEntity.TABLE} as s on s.cardName = t.cardName and s.characterId = t.fromCharacterId " +
             "where t.cardName = :cardName and t.toCharacterId = :toCharacterId")
-    fun getPhaseTransformationsToCharacter(cardName: String, toCharacterId: Int): Int?
+    fun getTransformationsToCharacterPhase(cardName: String, toCharacterId: Int): Int?
+
+    @Query("select min(s.phase) from ${TransformationEntity.TABLE} as t " +
+            "inner join ${SpeciesEntity.TABLE} as s on s.cardName = t.cardName and s.characterId = t.toCharacterId " +
+            "where t.cardName = :cardName and t.fromCharacterId = :fromCharacterId")
+    fun getNextTransformationPhaseFromCharacter(cardName: String, fromCharacterId: Int): Int?
 
     @Query("select max(s.phase) from ${SpecificFusionEntity.TABLE} as f " +
             "inner join ${SpeciesEntity.TABLE} as s on s.cardName = f.cardName and s.characterId = f.fromCharacterId " +
             "where f.cardName = :cardName and f.toCharacterId = :speciesId")
-    fun getFusionPhasesToCharacter(cardName: String, speciesId: Int): Int?
+    fun getFusionToCharacterPhase(cardName: String, speciesId: Int): Int?
+
+    @Query("select s.* from ${SpeciesEntity.TABLE} s " +
+            "inner join ${CardMetaEntity.TABLE} c on c.cardName = s.cardName and c.cardType = 'BEM' " +
+            "where s.spriteDirName = :spriteDir")
+    fun getSameSpeciesFromBEM(spriteDir: String): List<SpeciesEntity>
 }
