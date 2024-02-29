71c6c5dd839ba2fc6fb49e8114f80cb37cfa834b
cfogrady <cfogrady@gmail.com>
2024-02-28 23:11:46 -0500

    Fix broken constructor

--- app/src/main/java/com/github/cfogrady/vitalwear/VitalWearApp.kt
+++ app/src/main/java/com/github/cfogrady/vitalwear/VitalWearApp.kt
@@ -164,7 +164,7 @@ class VitalWearApp : Application(), Configuration.Provider {
         val stepIOService = StepIOService(sharedPreferences, stepState)
         saveService = SaveService(characterManager as CharacterManagerImpl, stepIOService, sharedPreferences)
         stepService = StepSensorService(sensorManager, sensorThreadHandler, Lists.newArrayList(vitalService), stepState, stepIOService, saveService)
-        moodService = MoodService(heartRateService, stepService, sensorManager, vbUpdater, characterManager, gameState)
+        moodService = MoodService(heartRateService, sensorManager, vbUpdater, characterManager, saveService)
         moodBroadcastReceiver = MoodBroadcastReceiver(moodService)
 
         trainingService = TrainingService(sensorManager, heartRateService, saveService)
