0e9a241ddc0c8b052475aec19d14488c7f017002
cfogrady <cfogrady@gmail.com>
2024-03-31 23:19:41 -0400

    Replace Loading Spinner

--- common/src/main/java/com/github/cfogrady/vitalwear/common/composable/util/LoadingComposable.kt
+++ common/src/main/java/com/github/cfogrady/vitalwear/common/composable/util/LoadingComposable.kt
@@ -1,14 +1,28 @@
 package com.github.cfogrady.vitalwear
 
+import androidx.compose.animation.core.KeyframesSpec
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.SnapSpec
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -16,16 +30,37 @@ import coil.ImageLoader
 import coil.compose.rememberAsyncImagePainter
 import coil.decode.ImageDecoderDecoder
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlin.coroutines.RestrictsSuspension
 
 const val LOADING_TEXT = "Loading..."
 
+@Preview
+@Composable
+fun TestLoading() {
+    Loading {
+
+    }
+}
+
 @Composable
 fun Loading(loadingText: String = LOADING_TEXT, scope: CoroutineDispatcher = Dispatchers.Default, work: suspend () -> Unit) {
-    val imageLoader = ImageLoader.Builder(LocalContext.current).components {
-        add(ImageDecoderDecoder.Factory())
-    }.build()
-
+    var targetValue by remember { mutableFloatStateOf(0f) }
+    val rotation by animateFloatAsState(
+        targetValue = targetValue,
+        animationSpec = SnapSpec(100),
+//        animationSpec = infiniteRepeatable(
+//            animation = tween(2500, easing = LinearEasing),
+//            repeatMode = RepeatMode.Restart),
+        label = "",
+        finishedListener = {
+            if (it == 360f) {
+                targetValue = 45f
+            } else {
+                targetValue += 45f
+            }
+        })
+    LaunchedEffect(true) {
+        targetValue = 45f
+    }
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
@@ -34,9 +69,11 @@ fun Loading(loadingText: String = LOADING_TEXT, scope: CoroutineDispatcher = Dis
     {
         Text(text = loadingText, color = Color.White)
         Image(
-            painter = rememberAsyncImagePainter(com.github.cfogrady.vitalwear.common.R.drawable.loading_icon, imageLoader),
+            painter = painterResource(id = com.github.cfogrady.vitalwear.common.R.drawable.loading_spinner),
             contentDescription = null,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .rotate(rotation)
         )
     }
     LaunchedEffect(key1 = loadingText) {
