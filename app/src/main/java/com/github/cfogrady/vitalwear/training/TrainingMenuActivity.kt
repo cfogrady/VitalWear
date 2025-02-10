c6a69580cbc5301b879ba886e12bc2932f988f3a
Christopher O'Grady <cfogrady@gmail.com>
2025-02-09 23:57:14 -0500

    Do permission checks before starting training.

--- app/src/main/java/com/github/cfogrady/vitalwear/training/TrainingMenuActivity.kt
+++ app/src/main/java/com/github/cfogrady/vitalwear/training/TrainingMenuActivity.kt
@@ -1,9 +1,15 @@
 package com.github.cfogrady.vitalwear.training
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -20,14 +26,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.wear.compose.material.Text
 import com.github.cfogrady.vitalwear.background.BackgroundManager
 import com.github.cfogrady.vitalwear.VitalWearApp
 import com.github.cfogrady.vitalwear.common.util.ActivityHelper
 import com.github.cfogrady.vitalwear.composable.util.BitmapScaler
-import com.github.cfogrady.vitalwear.util.flow.filterState
 import com.github.cfogrady.vitalwear.util.flow.transformState
+import timber.log.Timber
 
 class TrainingMenuActivity : ComponentActivity() {
 
@@ -35,9 +42,9 @@ class TrainingMenuActivity : ComponentActivity() {
     private lateinit var firmware: TrainingFirmwareSprites
     private lateinit var backgroundManager: BackgroundManager
 
-    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        doPermissionChecks()
         val activityHelper = ActivityHelper(this)
         bitmapScaler = (application as VitalWearApp).bitmapScaler
         firmware = (application as VitalWearApp).firmwareManager.getFirmware().value!!.trainingFirmwareSprites
@@ -53,7 +60,7 @@ class TrainingMenuActivity : ComponentActivity() {
         setContent {
             val background by backgroundManager.selectedBackground.transformState(backgroundOnLoad) {
                 if(it != null) {
-                    emit(it!!)
+                    emit(it)
                 }
             }.collectAsStateWithLifecycle()
             vitalBoxFactory.VitalBox {
@@ -107,5 +114,97 @@ class TrainingMenuActivity : ComponentActivity() {
             Text(text = "$timeInSeconds", fontWeight = FontWeight.Bold, fontSize = 4.em, color = Color.Yellow, modifier = Modifier.padding(top = 10.dp))
             Text(text = "sec", fontSize = 3.em, color = Color.Yellow)
         }
+    }
+
+    private fun doPermissionChecks() {
+        val missingPermissions = getMissingPermissionsNeeded()
+        val missingBackgroundPermissions = getMissingBackgroundPermissions()
+        val backgroundPermissionRequestLauncher = buildBackgroundPermissionRequestLauncher(missingBackgroundPermissions)
+        if(missingPermissions.isNotEmpty()) {
+            buildPermissionRequestLauncher { requestedPermissions->
+                val deniedPermissions = mutableListOf<String>()
+                for(requestedPermission in requestedPermissions) {
+                    if(!requestedPermission.value) {
+                        deniedPermissions.add(requestedPermission.key)
+                    }
+                }
+                if(deniedPermissions.isNotEmpty()) {
+                    Toast.makeText(this, "Permission Required For Training", Toast.LENGTH_SHORT).show()
+                    Timber.i("Permissions not provided for: $deniedPermissions")
+                    finish()
+                } else if(missingBackgroundPermissions.isNotEmpty()) {
+                    backgroundPermissionRequestLauncher.launch(missingBackgroundPermissions.toTypedArray())
+                }
+            }.launch(missingPermissions.toTypedArray())
+        } else if(missingBackgroundPermissions.isNotEmpty()) {
+            backgroundPermissionRequestLauncher.launch(missingBackgroundPermissions.toTypedArray())
+        }
+    }
+
+    private fun buildBackgroundPermissionRequestLauncher(missingBackgroundPermissions: List<String>): ActivityResultLauncher<Array<String>> {
+        return buildPermissionRequestLauncher { requestedPermissions->
+            val deniedPermissions = mutableListOf<String>()
+            for(requestedPermission in requestedPermissions) {
+                if(!requestedPermission.value) {
+                    deniedPermissions.add(requestedPermission.key)
+                }
+            }
+            if(deniedPermissions.isNotEmpty()) {
+                Toast.makeText(this, "Permission Required For Training", Toast.LENGTH_SHORT).show()
+                Timber.i("Permissions not provided for: $deniedPermissions")
+                finish()
+            }
+        }
+    }
+
+    private fun getMissingPermissionsNeeded(): List<String> {
+        val missingPermissions = mutableListOf<String>()
+        if(Build.VERSION.SDK_INT > 28) {
+            if (!hasPermission(Manifest.permission.ACTIVITY_RECOGNITION)) {
+                missingPermissions.add(Manifest.permission.ACTIVITY_RECOGNITION)
+            }
+        }
+        if (!hasPermission(Manifest.permission.BODY_SENSORS)) {
+            missingPermissions.add(Manifest.permission.BODY_SENSORS)
+        }
+        if (!hasPermission(Manifest.permission.WAKE_LOCK)) {
+            missingPermissions.add(Manifest.permission.WAKE_LOCK)
+        }
+        if (!hasPermission(Manifest.permission.FOREGROUND_SERVICE)) {
+            missingPermissions.add(Manifest.permission.FOREGROUND_SERVICE)
+        }
+
+        return missingPermissions
+    }
+
+    // backgound permissions must be requested after regular permissions of the same name
+    // or else the permission requester won't launch... for some reason. So do a separate
+    // check for background permissions.
+    private fun getMissingBackgroundPermissions(): List<String> {
+        val missingPermissions = mutableListOf<String>()
+        if(Build.VERSION.SDK_INT >= 33) {
+            if (!hasPermission(Manifest.permission.BODY_SENSORS_BACKGROUND)) {
+                missingPermissions.add(Manifest.permission.BODY_SENSORS_BACKGROUND)
+            }
+        }
+        if(Build.VERSION.SDK_INT >= 34) {
+            if (!hasPermission(Manifest.permission.FOREGROUND_SERVICE_HEALTH)) {
+                missingPermissions.add(Manifest.permission.FOREGROUND_SERVICE_HEALTH)
+            }
+        }
+        return missingPermissions
+    }
+
+    private fun hasPermission(permission: String): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun buildPermissionRequestLauncher(resultBehavior: (Map<String, Boolean>)->Unit): ActivityResultLauncher<Array<String>> {
+        val multiplePermissionsContract = ActivityResultContracts.RequestMultiplePermissions()
+        val launcher = registerForActivityResult(multiplePermissionsContract, resultBehavior)
+        return launcher
     }
 }
