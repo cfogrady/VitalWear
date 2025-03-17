c588d91d4182ab849f0d4e57274bc34660a2e262
Christopher O'Grady <cfogrady@gmail.com>
2025-03-17 10:15:23 -0400

    WIP: Remove Obsolete FirmwareSprite classes

--- app/src/main/java/com/github/cfogrady/vitalwear/training/TrainingMenuActivity.kt
+++ app/src/main/java/com/github/cfogrady/vitalwear/training/TrainingMenuActivity.kt
@@ -1,16 +1,9 @@
 package com.github.cfogrady.vitalwear.training
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -26,27 +19,26 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
-import androidx.core.app.ActivityCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.wear.compose.material.Text
 import com.github.cfogrady.vitalwear.background.BackgroundManager
 import com.github.cfogrady.vitalwear.VitalWearApp
 import com.github.cfogrady.vitalwear.common.util.ActivityHelper
 import com.github.cfogrady.vitalwear.composable.util.BitmapScaler
+import com.github.cfogrady.vitalwear.firmware.components.TrainingBitmaps
 import com.github.cfogrady.vitalwear.util.flow.transformState
-import timber.log.Timber
 
 class TrainingMenuActivity : ComponentActivity() {
 
     private lateinit var bitmapScaler: BitmapScaler
-    private lateinit var firmware: TrainingFirmwareSprites
+    private lateinit var firmware: TrainingBitmaps
     private lateinit var backgroundManager: BackgroundManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val activityHelper = ActivityHelper(this)
         bitmapScaler = (application as VitalWearApp).bitmapScaler
-        firmware = (application as VitalWearApp).firmwareManager.getFirmware().value!!.trainingFirmwareSprites
+        firmware = (application as VitalWearApp).firmwareManager.getFirmware().value!!.trainingBitmaps
         backgroundManager = (application as VitalWearApp).backgroundManager
         val vitalBoxFactory = (application as VitalWearApp).vitalBoxFactory
         val trainingLauncher = activityHelper.getActivityLauncherWithResultHandling(TrainingActivity::class.java) {
