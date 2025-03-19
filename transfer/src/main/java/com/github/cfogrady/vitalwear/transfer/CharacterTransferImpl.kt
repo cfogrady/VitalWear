56ac91fd03421051de9d8de984addefad43c3b89
Christopher O'Grady <cfogrady@gmail.com>
2025-03-18 22:23:09 -0400

    Multi version transfer support (#95)
    * Add protocol support for interoperability with multiple versions.
    
    * Switch var to val

--- transfer/src/main/java/com/github/cfogrady/vitalwear/transfer/CharacterTransferImpl.kt
+++ transfer/src/main/java/com/github/cfogrady/vitalwear/transfer/CharacterTransferImpl.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import com.github.cfogrady.nearby.connections.p2p.ConnectionStatus
 import com.github.cfogrady.nearby.connections.p2p.NearbyP2PConnection
 import com.github.cfogrady.vitalwear.protos.Character
+import com.github.cfogrady.vitalwear.protos.ConnectMeta
 import com.github.cfogrady.vitalwear.transfer.CharacterTransfer.Result
+import com.google.protobuf.InvalidProtocolBufferException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -20,11 +22,13 @@ import java.nio.charset.StandardCharsets
 internal class CharacterTransferImpl(context: Context): CharacterTransfer {
     companion object {
         const val TAG = "CharacterTransferImpl"
-        val READY_SIGNAL = "READY".toByteArray(StandardCharsets.UTF_8)
+        val READY_SIGNAL = "READY"
         val RECEIVED_SIGNAL = "RECEIVED".toByteArray(StandardCharsets.UTF_8)
         val REJECTED_SIGNAL = "REJECTED".toByteArray(StandardCharsets.UTF_8)
 
         const val TRANSFER_CHARACTER_SERVICE_ID = "com.github.cfogrady.vitalwear.transfer"
+
+        val SUPPORTED_VERSIONS = setOf(1)
 
     }
 
@@ -42,18 +46,23 @@ internal class CharacterTransferImpl(context: Context): CharacterTransfer {
     }
 
     override fun receiveCharacterFrom(senderName: String, receive: suspend (Character)->Boolean): StateFlow<Result> {
-        var result = MutableStateFlow(Result.TRANSFERRING)
+        val result = MutableStateFlow(Result.TRANSFERRING)
+        val packetsReceived = 0
         nearbyP2PConnection.onReceive = { payload ->
             CoroutineScope(Dispatchers.IO).launch {
-                val character = Character.parseFrom(payload.asBytes())
-                if(receive(character)) {
-                    Log.i(TAG, "Send Received Signal")
-                    nearbyP2PConnection.sendData(RECEIVED_SIGNAL)
-                    result.update { Result.SUCCESS }
-                } else {
-                    Log.i(TAG, "Send Rejected Signal")
-                    nearbyP2PConnection.sendData(REJECTED_SIGNAL)
-                    result.update { Result.REJECTED }
+                when(packetsReceived) {
+                    0 -> {
+                        val character = Character.parseFrom(payload.asBytes())
+                        if(receive(character)) {
+                            Log.i(TAG, "Send Received Signal")
+                            nearbyP2PConnection.sendData(RECEIVED_SIGNAL)
+                            result.update { Result.SUCCESS }
+                        } else {
+                            Log.i(TAG, "Send Rejected Signal")
+                            nearbyP2PConnection.sendData(REJECTED_SIGNAL)
+                            result.update { Result.REJECTED }
+                        }
+                    }
                 }
             }
         }
@@ -66,7 +75,11 @@ internal class CharacterTransferImpl(context: Context): CharacterTransfer {
             }.collect {
                 if(it == ConnectionStatus.CONNECTED) {
                     Log.i(TAG, "Send Ready Signal")
-                    nearbyP2PConnection.sendData(READY_SIGNAL)
+                    val connectMeta = ConnectMeta.newBuilder()
+                        .setReadyIndicator(READY_SIGNAL)
+                        .addAllSupportedVersions(listOf(1))
+                        .build()
+                    nearbyP2PConnection.sendData(connectMeta.toByteArray())
                 }
             }
             if(result.value == Result.TRANSFERRING) {
@@ -81,16 +94,44 @@ internal class CharacterTransferImpl(context: Context): CharacterTransfer {
         return result
     }
 
+    fun findMaxSharedVersion(otherVersions: Set<Int>): Int {
+        val matchingVersions = SUPPORTED_VERSIONS.intersect(otherVersions)
+        return matchingVersions.last()
+    }
+
     override fun sendCharacterToDevice(senderName: String, character: Character): StateFlow<Result> {
         val result = MutableStateFlow(Result.TRANSFERRING)
+        var packetsReceived = 0
         nearbyP2PConnection.onReceive = { payload ->
-            if(payload.asBytes().contentEquals(READY_SIGNAL)) {
-                Log.i(TAG, "Received Ready Signal. Sending Character")
-                nearbyP2PConnection.sendData(character.toByteArray())
-            } else if (payload.asBytes().contentEquals(RECEIVED_SIGNAL)) {
-                Log.i(TAG, "Received Success Signal.")
-                result.update { Result.SUCCESS }
-                close()
+            CoroutineScope(Dispatchers.IO).launch {
+                when(packetsReceived) {
+                    0 -> {
+                        try {
+                            val connectionMeta = ConnectMeta.parseFrom(payload.asBytes())
+                            if(READY_SIGNAL == connectionMeta.readyIndicator) {
+                                packetsReceived++
+                                // val useVersion = findMaxSharedVersion(connectionMeta.supportedVersionsList.toSet())
+                                Log.i(TAG, "Received Ready Signal. Sending Character")
+                                nearbyP2PConnection.sendData(character.toByteArray())
+                            }
+                        } catch (e: InvalidProtocolBufferException) {
+                            Log.i(TAG, "Expected ConnectMeta was invalid")
+                            result.update { Result.FAILURE }
+                            close()
+                        }
+                    }
+                    1 -> {
+                        if (payload.asBytes().contentEquals(RECEIVED_SIGNAL)) {
+                            Log.i(TAG, "Received Success Signal.")
+                            result.update { Result.SUCCESS }
+                            close()
+                        } else {
+                            Log.i(TAG, "Received Unexpected Signal.")
+                            result.update { Result.FAILURE }
+                            close()
+                        }
+                    }
+                }
             }
         }
         CoroutineScope(Dispatchers.IO).launch {
@@ -100,7 +141,7 @@ internal class CharacterTransferImpl(context: Context): CharacterTransfer {
                     result.update { Result.REJECTED }
                 }
                 it != ConnectionStatus.REJECTED && it != ConnectionStatus.DISCONNECTED && it != ConnectionStatus.IDLE
-            }.collect {}
+            }.collect {} // blocks until previous statement evaluates as false
             // disconnected
             close()
             if(result.value == Result.TRANSFERRING) {
